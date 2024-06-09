ff3d4390d05eddba512a317111df0a8f0a2623aa
BlackfiredDV3 <132904425+BlackfiredDV3@users.noreply.github.com>
2024-06-09 16:43:52 +0200

    he borrado cosas

---
+++ App.js
@@ -0,0 +1,10 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import MenuTabs from './src/navigation/MenuTabs';
+export default function App() {
+    return (
+        <NavigationContainer>
+            <MenuTabs />
+        </NavigationContainer>
+    );
+}
